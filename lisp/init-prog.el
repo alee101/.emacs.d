d5e479efcaab61aaabeb25888d4798526c5a04e9
albert <albert@email>
2023-12-29 14:27:44 -0800

    switch to using gopls w/eglot for go

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -1,11 +1,19 @@
 ;;; init-prog.el --- language specific init
 
-(use-package go-mode
-  :config
-  (progn
-    (setq gofmt-command "goimports")
-    (add-hook 'before-save-hook #'gofmt-before-save)))
+(use-package go-mode)
 
 
 (use-package restclient
   :mode ("\\.api\\'" . restclient-mode))
+
+
+(require 'eglot)
+(add-hook 'go-mode-hook 'eglot-ensure)
+(add-hook 'go-mode-hook 'eglot-go-on-save-hooks)
+
+(defun eglot-go-on-save-hooks ()
+  (add-hook 'before-save-hook 'eglot-organize-imports nil t)
+  (add-hook 'before-save-hook 'eglot-format-buffer -10 t))
+
+(defun eglot-organize-imports ()
+  (call-interactively 'eglot-code-action-organize-imports))
