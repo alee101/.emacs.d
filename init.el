fc29f1bfd0ce121b13df4d746acb792b952e5f00
albert <albert@email>
2024-03-01 22:13:55 -0800

    js config

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(restclient yasnippet wgrep-helm undo-tree smartparens s rainbow-delimiters paredit magit helm-swoop helm-projectile helm-git-grep helm-descbinds go-mode gited git-timemachine flycheck diminish darkroom company avy)))
+   '(js2-mode restclient yasnippet wgrep-helm undo-tree smartparens s rainbow-delimiters paredit magit helm-swoop helm-projectile helm-git-grep helm-descbinds go-mode gited git-timemachine flycheck diminish darkroom company avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -1,5 +1,21 @@
 ;;; init-prog.el --- language specific init
 
+(use-package js2-mode
+  :mode "\\.js\\'"
+  :config
+  (progn
+    (setq
+     js-indent-level 2
+     js2-basic-offset 2
+     js2-include-node-externs t
+     js2-strict-inconsistent-return-warning nil
+     js2-strict-trailing-comma-warning nil)))
+
+(custom-set-variables
+ '(js2-bounce-indent-p t)
+ '(js2-indent-switch-body t))
+
+
 (use-package go-mode)
 
 
